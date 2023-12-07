7d49af09b161849da3f57a08be6e40603efbc655
Julian Montague <juliangm@stud.ntnu.no>
2023-12-07 16:38:01 +0100

    Add /number/3 endpoint

--- django/numbersapp/views.py
+++ django/numbersapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
 
-# Create your views here.
+def get_number(num):
+    if isinstance(num, int):
+        return HttpResponse(str(num))
+    else:
+        return HttpResponseBadRequest('Numbers must be whole numbers (integers)')
+
+def get_3(request):
+    return get_number(3)
