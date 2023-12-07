c03468eac14a76f66c89c20144ad8634e97713ae
Julian Montague <juliangm@stud.ntnu.no>
2023-12-07 20:33:12 +0100

    Prepare Django settings for production
    Following Django's [Deployment checklist][1].
    
    I was originally going to use Docker Compose to load the secret key from
    a file, but after reading about [SECRET_KEY][2] I realize that it's only
    used for things that I'm not using. So I won't bother.
    
    [ALLOWED_HOSTS can't be empty without DEBUG = True][3], and I don't know
    what to set it to so I'm using the wildcard.
    
    [1]: https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
    [2]: https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-SECRET_KEY
    [3]: https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/#environment-specific-settings

--- django/addnumbers/settings.py
+++ django/addnumbers/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-_jqgh4(j97zz*@94wl7@-i7hq9c5_cbu@#rw=st$om=)gtm23g'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# SECURITY WARNING: this is vulnerable to Cross-Site Request Forging,
+# we're only using it because our project has no state
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/#allowed-hosts
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
