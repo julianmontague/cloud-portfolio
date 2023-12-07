ebef1903c13f169b221417b4cd654848fba0054e
Julian Montague <juliangm@stud.ntnu.no>
2023-12-07 17:27:39 +0100

    Add /number/4 and /number/any/* endpoints
    get_number() can be called directly with numbers < 0, but the int path
    converter will never match them.

--- django/numbersapp/urls.py
+++ django/numbersapp/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('3', views.get_3),
+    path('4', views.get_4),
+    path('any/<int:num>', views.get_number),
+    # if an int doesn't match, send it as a string
+    path('any/<num>', views.get_number),
 ]
--- django/numbersapp/views.py
+++ django/numbersapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest
 
-def get_number(num):
+def get_number(request, num):
     if isinstance(num, int):
         return HttpResponse(str(num))
     else:
-        return HttpResponseBadRequest('Numbers must be whole numbers (integers)')
+        return HttpResponseBadRequest('Numbers must be whole numbers (integers) greater than zero')
 
 def get_3(request):
-    return get_number(3)
+    return get_number(request, 3)
+
+def get_4(request):
+    return get_number(request, 4)
